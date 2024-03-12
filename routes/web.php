686ff41fb4b134e199770abd9c6f934761ecfaa3
Long Soeng <longsoeng017@gmail.com>
2024-03-12 15:30:50 +0700

    Fresh Project

---
+++ routes/web.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::group([
+    'middleware' => 'role:super-admin|admin'
+], function() {
+    Route::resource('permissions', PermissionController::class);
+    Route::get('permissions/{id}/delete', [PermissionController::class, 'destroy']);
+
+    Route::resource('roles', RoleController::class);
+    Route::get('roles/{id}/delete', [RoleController::class, 'destroy']);
+    Route::get('roles/{id}/give-permissions', [RoleController::class, 'givePermissionsToRole']);
+    Route::put('roles/{id}/give-permissions', [RoleController::class, 'updatePermissionsToRole']);
+
+    Route::resource('users', UserController::class);
+    Route::put('users/{user}/update-password', [UserController::class, 'updateUserPassword']);
+    Route::get('users/{user}/delete', [UserController::class, 'destroy']);
+});
+
+Route::get('ckeditor4-demo', function() {
+    return view('ckeditor-demo.ckeditor4-demo');
+})->name('ckeditor4');
+
+Route::get('ckeditor5-demo', function() {
+    return view('ckeditor-demo.ckeditor5-demo');
+})->name('ckeditor5');
+
+Route::get('slide-infinite-loop', function() {
+    return view('slide-show.slide-infinite-loop');
+})->name('slide-infinite-loop');
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
+
+
+
+
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
